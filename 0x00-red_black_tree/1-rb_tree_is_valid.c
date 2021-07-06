eeedf38351f6e79e862918ef7c6246458a8374d2
Athesto <gamez.live@gmail.com>
2021-07-06 12:46:03 -0500

    1-rb_tree_is_valid.c

--- 0x00-red_black_tree/1-rb_tree_is_valid.c
+++ 0x00-red_black_tree/1-rb_tree_is_valid.c
@@ -1,4 +1,7 @@
 #include "rb_trees.h"
+int isBST(const rb_tree_t *tree);
+int diffHeight(const rb_tree_t *tree);
+
 /**
  * rb_tree_is_valid - checks if a binary tree is a valid Red-Black Tree
  * @tree: a pointer to the root node of the tree to check
@@ -7,6 +10,56 @@
  */
 int rb_tree_is_valid(const rb_tree_t *tree)
 {
-	(void) tree;
+	if (!tree)
+		return (0);
+
+	if (tree->color != BLACK)
+		return (0);
+	if (!isBST(tree))
+		return (0);
+
+	if (diffHeight(tree) > 1)
+		return (0);
+
+	return (1);
+}
+/**
+ * diffHeight - is height-balanced binary tree
+ * @tree: head of the tree
+ * Return: 1 is true, 0 is false
+ *
+ */
+int diffHeight(const rb_tree_t *tree)
+{
+	int diff;
+
+	if (!tree)
+		return (-1);
+	diff = diffHeight(tree->left) - diffHeight(tree->right);
+	if (diff >= 0)
+		return (diff + 1);
+	return (-diff + 1);
+}
+
+/**
+ * isBST - is a binary search tree
+ * @tree: head of the tree;
+ * Return: 1 is true, 0 is false
+ */
+int isBST(const rb_tree_t *tree)
+{
+	if (!tree)
+		return (1);
+
+	if (isBST(tree->left) && isBST(tree->right))
+	{
+		if (tree->left && tree->right)
+			return (tree->left->n < tree->n && tree->n < tree->right->n);
+		if (tree->left)
+			return (tree->left->n < tree->n);
+		if (tree->right)
+			return (tree->n < tree->right->n);
+		return (1);
+	}
 	return (0);
 }
